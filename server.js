ff3bba96744556995bc80d5bac85c10e72a3ee92
Alieu Ndimbalane <alieu.itg@gmail.com>
2022-01-20 19:56:53 +0000

    v1.0.1

--- server.js
+++ server.js
@@ -3,38 +3,36 @@ const path = require('path')
 const fs = require('fs')
 const axios = require('axios')
 const app = express();
+const cors = require('cors')
 
-app.use(express.static('public'))
-app.use(express.static('exports'))
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'export')));
 app.use(express.urlencoded({extended:false}))
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname ,'views/index.html'))
-})
 
-app.get('/export/:id', (req,res) => {
-    fs.readdir(path.join(__dirname, 'exports'), (err, files) => {
+app.post('/export', (req,res) => {
+    fs.readdir(path.join(__dirname, 'export'), (err, files) => {
         if(err) throw err;
         for(const file of files){
-            fs.unlink(path.join(__dirname, 'exports', file), err => {
+            fs.unlink(path.join(__dirname, 'export', file), err => {
                 if(err) throw err;
                 console.log('File deleted')
             })
         }
     })
-    let nodeId = req.params.id;
-    axios.get(`http://localhost:3333/notes/${nodeId}`)
-        // .then(data => data.json())
-        .then(({data}) => {
-            console.log(data.title)
-            fs.appendFile(path.join(__dirname, `exports/${data.title}.md`), `---\ntitle: ${data.title}\ncreatedAt: ${data.date}\n---\n\n${data.content}`, err => {
-                if(err) throw err;
-                // console.log('saved')
-                // res.redirect(`/${data.note.title}.md`)
-                res.download(path.join(__dirname, `exports/${data.title}.md`))
-            })
-        })
-        .catch(err => console.log(err));
+    let data = req.body
+    let returnPath = ''
+     fs.appendFile(path.join(__dirname, `export/${data.title}.md`), `---\ntitle: ${data.title}\ncreatedAt: ${data.date}\n---\n\n${data.content}`, err => {
+       if(err) throw err;
+       // console.log('saved')
+       // res.redirect(`/${data.note.title}.md`)
+       returnPath = path.join(__dirname, `export/${data.title}.md`)
+       res.status(201).json({
+         message:`${data.title}.md`
+       })
+     })
+
 })
 
-let port = process.env.PORT || 3000
+let port = 5000
 app.listen(port, () => console.log(`App running on port ${port}`))
